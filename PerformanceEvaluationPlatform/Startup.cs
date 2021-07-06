811364956583bea16b02e0c539e64f81683f7fce
Oleg Bezhan <oleg.bezhan@gmail.com>
2021-07-06 16:14:36 +0000

    Merged PR 5: Fix typo in Solution name
    Fix typo in Solution name

--- PerformanceEvaluationPlatform/Startup.cs
+++ PerformanceEvaluationPlatform/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace PerfomanceEvaluationPlatform
+namespace PerformanceEvaluationPlatform
 {
     public class Startup
     {
