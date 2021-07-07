40e0c52357c3b54e5a45903f55a28d7384065bd1
Vlad Zaremskyi <zaremskyivlad@gmail.com>
2021-07-07 16:08:51 +0300

    CreateActionGetUsers

--- PerformanceEvaluationPlatform/Controllers/UsersController.cs
+++ PerformanceEvaluationPlatform/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PerformanceEvaluationPlatform.Models.User.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,32 @@ namespace PerformanceEvaluationPlatform.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class UsersController :ControllerBase
+    public class UsersController : ControllerBase
     {
+        private static List<UserViewModel> users;
+        public UsersController()
+        {
+            users = new List<UserViewModel>()
+            {
+                new UserViewModel(){Email = "userExample@gor.com",FirstName ="Artur",LastName = "Grugon",Level = "Junior",
+                LevelId =1,PreviousPEDate = new DateTime(2021,05,03),State = "Active",StateId=1,TeamName = "Sharks",
+                Role = "Dev", RoleId = 1
+                },
+                    new UserViewModel(){Email = "GodKiller@gmail.com",FirstName ="Kiril",LastName = "Krigan",Level = "Senior",
+                LevelId =3,PreviousPEDate = new DateTime(2021,04,10),State = "Active",StateId=1,TeamName = "Gnomes",
+                Role = "Architector", RoleId = 2
+                },
+                        new UserViewModel(){Email = "bestmanager@ukr.net",FirstName ="Kristina",LastName = "Lavruk",Level = "Junior",
+                LevelId =1,PreviousPEDate = new DateTime(2021,03,07),State = "Active",StateId=1,TeamName = "Sharks",
+                Role = "Dev", RoleId = 1}
+            };
+        }
 
+        //GET api/users
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok(users);
+        }
     }
 }
---
+++ PerformanceEvaluationPlatform/Models/User/ViewModels/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerformanceEvaluationPlatform.Models.User.ViewModels
+{
+    public class UserViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        //Повиннен получати доступ до імені команди з змінної типу Team, поки що так 
+        public string TeamName { get; set; }
+
+        public int StateId { get; set; }
+        public string State { get; set; }
+
+        public int LevelId { get; set; }
+        public string Level { get; set; }
+
+        public int RoleId { get; set; }
+        public string Role { get; set; }
+
+        public DateTime PreviousPEDate { get; set; }
+        public DateTime NextPEDate { get 
+            {
+                return PreviousPEDate.AddMonths(5);
+            } } 
+
+    }
+}
