191eab17ef2cfa508741d567dce8aa680c45737c
Yuliia-077 <62853554+Yuliia-077@users.noreply.github.com>
2021-07-08 14:41:50 +0300

    create form templates controller and view model

---
+++ PerformanceEvaluationPlatform/Controllers/FormTemplatesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PerformanceEvaluationPlatform.Models.FormTemplates.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerformanceEvaluationPlatform.Controllers
+{
+    [ApiController]
+    public class FormTemplatesController: ControllerBase
+    {
+        [Route("formtemplates")]
+        public IActionResult Get()
+        {
+            var items = GetFormTemplatesListItemViewModel();
+            return Ok(items);
+        }
+
+        private IEnumerable<FormTemplatesListItemViewModel> GetFormTemplatesListItemViewModel()
+        {
+            var items = new List<FormTemplatesListItemViewModel> { 
+                new FormTemplatesListItemViewModel{
+                    Name = "Middle Back-End Dev",
+                    Version = "12",
+                    Status = "Draft",
+                    AssesmentGroup = "A-F Marks",
+                    CreatedAt = new DateTime(2021, 7, 1, 9, 15, 0)
+                },
+                new FormTemplatesListItemViewModel{
+                    Name = "Middle Front-End Dev",
+                    Version = "1",
+                    Status = "Active",
+                    AssesmentGroup = "A-F Marks",
+                    CreatedAt = new DateTime(2021, 7, 1, 9, 15, 0)
+                },
+            };
+            return items;
+        }
+    }
+}
---
+++ PerformanceEvaluationPlatform/Models/FormTemplates/ViewModels/FormTemplatesListItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerformanceEvaluationPlatform.Models.FormTemplates.ViewModels
+{
+    public class FormTemplatesListItemViewModel
+    {
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+        // can be enum
+        public string Status { get; set; }
+        // change to class
+        public string AssesmentGroup { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
