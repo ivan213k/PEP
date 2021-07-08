2390f806b8aa7122773d779f6bb50b000d76e814
Vlad Zaremskyi <zaremskyivlad@gmail.com>
2021-07-08 16:39:38 +0300

    FixIssues

--- PerformanceEvaluationPlatform/Controllers/UsersController.cs
+++ PerformanceEvaluationPlatform/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PerformanceEvaluationPlatform.Models.User.Domain;
 using PerformanceEvaluationPlatform.Models.User.RequestModels;
 using PerformanceEvaluationPlatform.Models.User.ViewModels;
 using System;
@@ -9,100 +10,109 @@ using System.Threading.Tasks;
 namespace PerformanceEvaluationPlatform.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class UsersController : ControllerBase
     {
-        private static List<UserViewModel> users;
-        public UsersController()
-        {
-            users = new List<UserViewModel>()
+        private static List<User> users = new List<User>()
             {
-                new UserViewModel(){Id = 1,Email = "userExample@gor.com",FirstName ="Artur",LastName = "Grugon",Level = "Junior",
-                LevelId =1,PreviousPEDate = new DateTime(2021,05,03),State = "Active",StateId=1,TeamName = "Sharks",
-                Role = "Dev", RoleId = 1
-                },
-                    new UserViewModel(){Id =2,Email = "GodKiller@gmail.com",FirstName ="Kiril",LastName = "Krigan",Level = "Senior",
-                LevelId =3,PreviousPEDate = new DateTime(2021,04,10),State = "Active",StateId=1,TeamName = "Gnomes",
-                Role = "Architector", RoleId = 2
-                },
-                        new UserViewModel(){Id=3,Email = "bestmanager@ukr.net",FirstName ="Kristina",LastName = "Lavruk",Level = "Junior",
-                LevelId =1,PreviousPEDate = new DateTime(2021,03,07),State = "Active",StateId=1,TeamName = "Sharks",
-                Role = "Dev", RoleId = 1}
-            };
-        }
+                new User(){Id = 1,Email = "userExample@gor.com",FirstName ="Artur",LastName = "Grugon",LevelName = "Junior",
+                LevelId =1,PreviousPEDate = new DateTime(2020,12,03),StateName = "Active",StateId=1,TeamName = "Sharks",RoleName = "Dev", RoleId = 1,
+                 EnglishLevelId=1,EnglishLevelName ="Proficiency",FirstDayInCompany = new DateTime(2020,03,07),ProjectName="PO for Progars",ProjectId=1,NextPEDate = new DateTime(2021,08,21),
+                        YearsOfExpirience=6,PreviousPEs = new List<DateTime>(){
+                            new DateTime(2020,06,20),
+                            new DateTime(2020,12,03)
+                } },
 
-        //GET api/users
+                    new User(){Id =2,Email = "KodKiller@gmail.com",FirstName ="Kiril",LastName = "Krigan",LevelName = "Senior",
+                LevelId =3,PreviousPEDate = new DateTime(2021,04,10),StateName = "Active",StateId=1,TeamName = "Gnomes",RoleName = "Architector", RoleId = 2,
+                EnglishLevelId=1,EnglishLevelName ="Advanced",FirstDayInCompany = new DateTime(2019,03,07),ProjectName="PO for Progars",ProjectId=1,NextPEDate = new DateTime(2022,06,21),
+                        YearsOfExpirience=6,PreviousPEs = new List<DateTime>(){
+                            new DateTime(2020,06,20),
+                            new DateTime(2021,04,10)
+                        } },
+
+                        new User(){Id=3,Email = "bestmanager@ukr.net",FirstName ="Kristina",LastName = "Lavruk",LevelName = "Junior",
+                LevelId =1,PreviousPEDate = new DateTime(2021,03,07),StateName = "Active",StateId=1,TeamName = "Sharks",RoleName = "Dev", RoleId = 1,
+                        EnglishLevelId=1,EnglishLevelName ="Intermidiate",FirstDayInCompany = new DateTime(2016,03,07),ProjectName="PO for Progars",ProjectId=1,NextPEDate = new DateTime(2022,03,07),
+                        YearsOfExpirience=6,PreviousPEs = new List<DateTime>(){
+                            new DateTime(2017,03,07),
+                            new DateTime(2018,03,07),
+                            new DateTime(2019,03,07),
+                            new DateTime(2020,03,07),
+                            new DateTime(2021,03,07)
+                        } }
+            };
+        
+
         [HttpGet]
-        public IActionResult GetUsers([FromQuery] UserSortingRequestModel userSorting,[FromQuery] UserFilterRequestModel userFilter,[FromQuery] PaginationFilterRequestModel userPagination)
+        public IActionResult Get([FromQuery] UserSortingRequestModel userSorting,[FromQuery] UserFilterRequestModel userFilter)
         {
            var items=  SortingUsers(userSorting,users);
             items = FilterUsers(userFilter, items);
-            return Ok(items.Skip((userPagination.CurrentPage - 1) * userPagination.PageSize).Take(userPagination.PageSize));
-        }
-        [HttpGet("roles")]
-        public IActionResult GetRoles()
-        {
-            var roles = new List<RolesViewModel>()
-            {
-            new RolesViewModel(){Id= 1,Name="Dev"},
-            new RolesViewModel(){Id= 2,Name="Architector"}
-            };
-            return Ok(roles);
+            var resultItems = MapToUserViewModel(items);
+            return Ok(resultItems.Skip((userFilter.Skip - 1) * userFilter.Take).Take(userFilter.Take));
         }
 
-        [HttpGet("levels")]
-        public IActionResult GetLeveles()
+        private IEnumerable<UserViewModel> MapToUserViewModel(IEnumerable<User> items)
         {
-            var roles = new List<LevelesViewModel>()
+            List<UserViewModel> users = new List<UserViewModel>();
+            foreach(var item in items)
             {
-            new LevelesViewModel(){Id= 1,Name="Junior"},
-            new LevelesViewModel(){Id= 2,Name="Middle"},
-            new LevelesViewModel(){Id= 3,Name="Senior"}
-            };
-            return Ok(roles);
+                users.Add(new UserViewModel()
+                {
+                    Id = item.Id,
+                    Email = item.Email,
+                    FirstName = item.FirstName,
+                    LastName = item.LastName,
+                    LevelName = item.LevelName,
+                    LevelId = item.LevelId,
+                    PreviousPEDate = item.PreviousPEDate,
+                    NextPEDate = item.NextPEDate,
+                    StateName = item.StateName,
+                    StateId = item.StateId,
+                    TeamName = item.TeamName,
+                    RoleName = item.RoleName,
+                    RoleId = item.RoleId
+                });
+            }
+            return users;
         }
 
-
-        private IEnumerable<UserViewModel> FilterUsers(UserFilterRequestModel userFilter, IEnumerable<UserViewModel> items)
+        private IEnumerable<User> FilterUsers(UserFilterRequestModel userFilter, IEnumerable<User> items)
         {
-            if(userFilter.EmailFilter != null)
+            if(userFilter.EmailOrName != null)
             {
-                items = items.Where(s => s.Email.Contains(userFilter.EmailFilter));
+                items = items.Where(s => s.Email.ToLower().Contains(userFilter.EmailOrName.ToLower()) || $"{s.FirstName} {s.LastName}".ToLower().Contains(userFilter.EmailOrName.ToLower()));
             }
 
-            if (userFilter.FullNameFilter != null)
+            if (userFilter.NextPEDate != null)
             {
-                items = items.Where(s => $"{s.FirstName} {s.LastName}".Contains(userFilter.FullNameFilter));
+                items = items.Where(s => s.NextPEDate <= userFilter.NextPEDate);
             }
 
-            if (userFilter.NextPEDateFilter != null)
+            if (userFilter.PreviousPEDate != null)
             {
-                items = items.Where(s => s.NextPEDate == userFilter.NextPEDateFilter);
+                items = items.Where(s => s.PreviousPEDate == userFilter.PreviousPEDate);
             }
 
-            if (userFilter.PreviousPEDateFilter != null)
+            if (userFilter.RoleIds != null)
             {
-                items = items.Where(s => s.PreviousPEDate == userFilter.PreviousPEDateFilter);
+                items = items.Where(s => userFilter.RoleIds.Contains(s.RoleId));
             }
 
-            if (userFilter.RoleIdFilter != null)
+            if (userFilter.StateIds != null)
             {
-                items = items.Where(s => s.RoleId == userFilter.RoleIdFilter);
-            }
-
-            if (userFilter.StateIdFilter != null)
-            {
-                items = items.Where(s => s.StateId == userFilter.StateIdFilter);
+                items = items.Where(s => userFilter.StateIds.Contains(s.StateId));
             }
 
             return items;
         }
 
-        private IEnumerable<UserViewModel> SortingUsers(UserSortingRequestModel userSorting,IEnumerable<UserViewModel> items)
+        private IEnumerable<User> SortingUsers(UserSortingRequestModel userSorting,IEnumerable<User> items)
         {
             if(userSorting != null)
             {
-                switch (userSorting.UserNameSorting)
+                switch (userSorting.UserName)
                 {
                     case 0:
                             break;
@@ -114,7 +124,7 @@ namespace PerformanceEvaluationPlatform.Controllers
                         break;
                 }
 
-                switch (userSorting.UserNextPESorting)
+                switch (userSorting.UserNextPE)
                 {
                     case 0:
                         break;
@@ -126,7 +136,7 @@ namespace PerformanceEvaluationPlatform.Controllers
                         break;
                 }
 
-                switch (userSorting.UserPreviousPESorting)
+                switch (userSorting.UserPreviousPE)
                 {
                     case 0:
                         break;
