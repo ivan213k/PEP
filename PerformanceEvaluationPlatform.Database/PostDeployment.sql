aad39fdd2105a93f973fea165e0803154fd5fe8b
Oleg Bezhan <oleg.bezhan@gmail.com>
2021-07-09 06:21:29 +0000

    Merged PR 28: Add database project
    Add database project

---
+++ PerformanceEvaluationPlatform.Database/PostDeployment.sql
@@ -0,0 +1,15 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+:r .\Example\PostDeployment\01.ExampleTypes.sql
+:r .\Example\PostDeployment\02.ExampleStates.sql
+:r .\Example\PostDeployment\03.Examples.sql
