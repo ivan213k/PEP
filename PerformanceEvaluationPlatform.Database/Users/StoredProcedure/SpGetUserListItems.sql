487146d548215ab0af786c66ee940ce1d7771934
Vlad Zaremskyi <zaremskyivlad@gmail.com>
2021-07-21 19:05:34 +0300

    FixNames

--- PerformanceEvaluationPlatform.Database/Users/StoredProcedure/SpGetUserListItems.sql
+++ PerformanceEvaluationPlatform.Database/Users/StoredProcedure/SpGetUserListItems.sql
@@ -144,8 +144,8 @@ Begin
 
 
 
-	DECLARE @Sql NVARCHAR(MAX)= 'SELECT DISTINCT [U].[Id],[U].[FirstName],[U].[LastName],[U].[Email],[US].[Name] AS [User State],
-	[T].[Title] as [Team Name], [R].[Title] as [Role Name],[L].[Name] AS [Level Name],[UPES].[PreviousPE], [UPES].[NextPE]
+	DECLARE @Sql NVARCHAR(MAX)= 'SELECT DISTINCT [U].[Id],[U].[FirstName],[U].[LastName],[U].[Email],[US].[Name] AS [UserState],
+	[T].[Title] as [TeamName], [R].[Title] as [RoleName],[L].[Name] AS [LevelName],[UPES].[PreviousPE], [UPES].[NextPE]
 	FROM [dbo].[User] AS [U]
 	INNER JOIN [dbo].[UserState] AS [US] ON [U].StateId = [US].[Id] 
 	INNER JOIN [dbo].[Team] AS [T] ON [T].Id = [U].[TeamId]
