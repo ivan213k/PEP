d913352b100cc588b5a0f29a09053feba4dc00d2
Miroslav Nagrodsky <dzodong@gmail.com>
2021-08-05 10:42:36 +0000

    Merged PR 131: Add create and edit Project
    Add create and edit Project

--- PerformanceEvaluationPlatform/Models/Project/RequestModels/CreateProjectRequestModel.cs
+++ PerformanceEvaluationPlatform/Models/Project/RequestModels/CreateProjectRequestModel.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PerformanceEvaluationPlatform.Models.Project.RequestModels
 {
     public class CreateProjectRequestModel
     {
+        [Required]
+        [MaxLength(128)]
         public string Title { get; set; }
+
+        [Required]
         public DateTime StartDate { get; set; }
-        public string CoordinatorId { get; set; }
+
+        [Required]
+        public int CoordinatorId { get; set; }
     }
 }
