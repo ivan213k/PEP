f52f074b4f169dab3b6e7030de284f3bf475f8a2
Dmytro Radchenko <hiichakii@gmail.com>
2021-08-19 06:21:49 +0000

    Merged PR 152: Add excel service/models
    Add excel service(only structure)/models and NPOI package to Application layer

---
+++ PerformanceEvaluationPlatform.Application/Model/Excel/ReportFieldDataAssessmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PerformanceEvaluationPlatform.Application.Model.Excel
+{
+    public class ReportFieldDataAssessmentDto
+    {
+        public string ReporterName { get; set; }
+        public string AssessmentName { get; set; }
+        public string Comment { get; set; }
+    }
+}
